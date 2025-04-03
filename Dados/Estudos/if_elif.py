26c887bb43600a9d5dd3b517b90a766dd142c3d2
Emerson Nakada <emersonnakada@gmail.com>
2025-04-02 23:59:19 -0300

    Aula6 IF ELSE

--- Dados/Estudos/if_elif.py
+++ Dados/Estudos/if_elif.py
@@ -6,4 +6,4 @@ quantidade_vendida = 2435400
 if quantidade_vendida > meta:
     print ('batemos a meta este mes, vendemos {} unidade!!!!!!!!!' .format(quantidade_vendida))
 
-print ('fim do programa')
+print ('fim do programa!!')
