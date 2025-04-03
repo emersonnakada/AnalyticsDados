9e523308a6f37dda28fb797511d6377baaf7e0e3
Emerson Nakada <emersonnakada@gmail.com>
2025-04-02 21:38:51 -0300

    primeiro commit

---
+++ Dados/Estudos/if_elif.py
@@ -0,0 +1,9 @@
+
+meta = 50000
+
+quantidade_vendida = 2435400
+
+if quantidade_vendida > meta:
+    print ('batemos a meta este mes, vendemos {} unidade!!!!!!!!!' .format(quantidade_vendida))
+
+print ('fim do programa')
--- Dados/etl;.py
+++ Dados/etl;.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import logging
 from datetime import datetime
